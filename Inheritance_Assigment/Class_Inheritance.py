b2614b9b1011abd8664ecbb1838c2b479522fcb7
Leonardo S Salazar <steven.salazar583@gmail.com>
2022-12-28 14:45:12 -0800

    Added a new folder
    Added a new folder containing a python file in which two child classes inherit the structure and behavior of a parent class.

---
+++ Inheritance_Assigment/Class_Inheritance.py
@@ -0,0 +1,13 @@
+class Vehicles:
+    brand: ''
+    year: ''
+    number_of_seats: ''
+    energy_source: ''
+
+class Plane(Vehicles):
+    wing_number: 2
+    engine_quantity: 4
+
+class Bike(Vehicles):
+    pedal_number: 2
+    terrain_type: 'dirt'
